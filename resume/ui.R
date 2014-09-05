b24f5683f32bbb08ccc416fb1b9ec62a85067f16
ivan-liuyanfeng <ivan.liuyanfeng@gmail.com>
2014-09-05 14:45:08 +1000

    setup

--- resume/ui.R
+++ resume/ui.R
@@ -6,6 +6,8 @@ shinyUI(navbarPage("AfterPlus Product Report", inverse = FALSE, collapsable = FA
                    tabPanel("Summary",
                             fluidRow(
                                 sidebarPanel(
+                                    helpText("Tips:"),
+                                    helpText("This is a summary re all diamonds information in our diamonds dataset. It has been visualized by a point plot on the right side. You are able to customize your own summary by selecting different variables you want to explore on x or y axis. You can also choose the categories of diamonds to reflect them on the diagram. Please make the adjustments below."),
                                     selectInput(inputId = "x",
                                                 label = "Choose X",
                                                 choices = c('clarity', 'depth', 'price', 'carat'),
@@ -35,8 +37,10 @@ shinyUI(navbarPage("AfterPlus Product Report", inverse = FALSE, collapsable = FA
                    tabPanel("Detailed Report",
                             sidebarLayout(
                                 sidebarPanel(
-                                        checkboxGroupInput('show_vars_tb1','Columns in Dongdamen to show:',
-                                                           names(diamonds), selected = names(diamonds))       
+                                    helpText("Tips:"),
+                                    helpText("This page allow you to view the details of diamonds based on different cut types. You can select the variables you want to include in the table. You can sort table by different variables and search record based on your preferences. You can also determine how many records you want to show on one page."),
+                                    checkboxGroupInput('show_vars_tb1','Columns in Dongdamen to show:',
+                                                        names(diamonds), selected = names(diamonds))       
                                 ),
                                 mainPanel(
                                     tabsetPanel(
@@ -53,6 +57,8 @@ shinyUI(navbarPage("AfterPlus Product Report", inverse = FALSE, collapsable = FA
                               tabPanel("Download Datasets",
                                        sidebarLayout(
                                            sidebarPanel(
+                                               helpText("Tips:"),
+                                               helpText("You can download the table for diamonds information here. Please select a type of diamonds that you are interested first and ensure the right table is the one you are willing to download. Then click the download button below. It will be a .csv format file on your drive."),
                                                selectInput("datasetD", "Choose a dataset:", 
                                                            choices = c("Fair Cut", "Good Cut", 
                                                                        "Very Good Cut", "Premium Cut", 
@@ -66,7 +72,8 @@ shinyUI(navbarPage("AfterPlus Product Report", inverse = FALSE, collapsable = FA
                               tabPanel("Market Information", 
                                        sidebarLayout(
                                            sidebarPanel(
-                                               helpText("Select a currency. Information will be collected from yahoo finance."),
+                                               helpText("Tips:"),
+                                               helpText("You can obtain the latest exchange rate regarding your selected currency from the right chart. Please select a currency type first. Information will be collected from yahoo finance."),
                                                textInput("symb", "Symbol", "AUD"),
                                                dateRangeInput("dates", "Date range", 
                                                               start = "2014-01-01", 
